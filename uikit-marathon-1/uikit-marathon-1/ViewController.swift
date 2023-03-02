a6a3ec39e84f594eb747d4cdf628f02203fcdecd
Timur Taneev <taneev@yandex.ru>
2023-03-02 22:14:14 +0100

    добавил размытие тени и констрейнты автолейаут для вью с градиентом

--- uikit-marathon-1/uikit-marathon-1/ViewController.swift
+++ uikit-marathon-1/uikit-marathon-1/ViewController.swift
@@ -16,10 +16,11 @@ class ViewController: UIViewController {
         shadowView.layer.masksToBounds = false
         shadowView.layer.shadowColor = UIColor.black.cgColor
         shadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        shadowView.layer.shadowRadius = 5
         shadowView.layer.shadowOpacity = 0.7
         shadowView.layer.shadowPath = UIBezierPath(roundedRect: shadowView.bounds, cornerRadius: 20).cgPath
         
-        // основная вью с закругленными углами
+        // "верхняя" вью с закругленными углами
         let roundedView = UIView(frame: shadowView.bounds)
         roundedView.layer.cornerRadius = 20
         roundedView.layer.masksToBounds = true
@@ -34,13 +35,17 @@ class ViewController: UIViewController {
         shadowView.addSubview(roundedView)
         view.addSubview(shadowView)
         
-        // фиксируем расположение
+        // фиксируем расположение с помощью Autolayout
         shadowView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             shadowView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             shadowView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             shadowView.widthAnchor.constraint(equalToConstant: 100),
-            shadowView.heightAnchor.constraint(equalToConstant: 100)
+            shadowView.heightAnchor.constraint(equalToConstant: 100),
+            roundedView.leadingAnchor.constraint(equalTo: shadowView.leadingAnchor),
+            roundedView.topAnchor.constraint(equalTo: shadowView.topAnchor),
+            roundedView.heightAnchor.constraint(equalTo: shadowView.heightAnchor),
+            roundedView.widthAnchor.constraint(equalTo: shadowView.widthAnchor)
         ])
     }
 }
